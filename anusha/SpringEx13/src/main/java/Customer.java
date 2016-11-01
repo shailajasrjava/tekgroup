b65f8724db8cc07e657b2097484a403e8ff7639f
raj kumar vallabhaneni <raj kumar vallabhaneni>
2016-11-01 17:38:29 -0400

    spring13added

---
+++ anusha/SpringEx13/src/main/java/Customer.java
@@ -0,0 +1,19 @@
+import java.util.Map;
+
+public class Customer {
+	private Map maps;
+
+	public Map getMaps() {
+		return maps;
+	}
+
+	public void setMaps(Map maps) {
+		this.maps = maps;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [maps=" + maps + "]";
+	}
+
+}
