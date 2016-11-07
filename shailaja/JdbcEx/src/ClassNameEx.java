765ec2423093818327ba3438cbf4275c876e047d
shailu123 <shailajabattu@gmail.com>
2016-11-06 21:03:50 -0500

    jdbc added

---
+++ shailaja/JdbcEx/src/ClassNameEx.java
@@ -0,0 +1,13 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ClassNameEx {
+	public static void main(String[] args) throws Exception {
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		Connection obj=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "system", "shailaja");
+		System.out.println(obj.getClass().getName());
+		ClassNameEx t=new ClassNameEx();
+		System.out.println(t.getClass().getName());
+	
+}
+}
