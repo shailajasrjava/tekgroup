a61a6b3d7def4c7149c961fce97d9bad6897ad20
tekgroup <priyankas.java@gmail.comt>
2016-12-28 16:21:34 -0600

    Added

---
+++ priyanka/CamelHelloWorld/src/main/java/com/tek/camel/CamelSpring.java
@@ -0,0 +1,24 @@
+package com.tek.camel;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.spring.SpringCamelContext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class CamelSpring {
+	public static void main(String[] args) throws Exception {
+		ApplicationContext appContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+		CamelContext camelContext = SpringCamelContext.springCamelContext(appContext, false);
+		try {
+			ProducerTemplate template = camelContext.createProducerTemplate();
+			camelContext.start();
+			template.sendBody("activemq:test.queue", "Hello World Camel");
+			Thread.sleep(2000);
+		} finally {
+			camelContext.stop();
+		}
+	}
+}
+
+
---
+++ priyanka/CamelHelloWorld/src/main/java/com/tek/camel/MyBean.java
@@ -0,0 +1,7 @@
+package com.tek.camel;
+
+public class MyBean {
+	public String appendCamel(String msg) {
+		return msg + " Camel1";
+	}
+}
