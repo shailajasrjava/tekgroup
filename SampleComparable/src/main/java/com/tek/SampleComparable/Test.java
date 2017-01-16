f1be66b72b289ed3d3783d9a67f884194738afcd
sonygutta <kalyanig.java@gmail.com>
2017-01-16 00:09:39 -0500

    Rehman 112 added.

--- SampleComparable/src/main/java/com/tek/SampleComparable/Test.java
+++ SampleComparable/src/main/java/com/tek/SampleComparable/Test.java
@@ -14,6 +14,7 @@ public class Test
 		al.add(new Student(121, "Ram", 32));
 		al.add(new Student(127, "Ravi", 62));
 		al.add(new Student(111, "Rehman", 786));
+		al.add(new Student(112, "Rehman", 786));
 		Collections.sort(al);
 		for(Student s:al)
 		{
