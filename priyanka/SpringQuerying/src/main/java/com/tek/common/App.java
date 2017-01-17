83a57afa59785bbf2e33efd1241686784fb1d6f3
tekgroup <priyankas.java@gmail.comt>
2017-01-16 22:24:03 -0600

    added

---
+++ priyanka/SpringQuerying/src/main/java/com/tek/common/App.java
@@ -0,0 +1,13 @@
+package com.tek.common;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	//...
+    	
+    }
+}
+
+
---
+++ priyanka/SpringQuerying/src/main/java/com/tek/customer/model/Customer.java
@@ -0,0 +1,54 @@
+package com.tek.customer.model;
+
+public class Customer {
+
+	public Customer(int i, String string, int j) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Customer() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Object getCustId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getAge() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void setCustId1(int int1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setName(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setAge(int int1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setCustId(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setCustId(Long long1) {
+		// TODO Auto-generated method stub
+		
+	}
+}
+
+
